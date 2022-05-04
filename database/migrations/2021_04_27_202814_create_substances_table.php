8415f63e660932b67e50371f46264c8888b6c287
Shift <shift@laravelshift.com>
2022-05-04 12:40:55 +0000

    Adopt anonymous migrations

--- database/migrations/2021_04_27_202814_create_substances_table.php
+++ database/migrations/2021_04_27_202814_create_substances_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSubstancesTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -33,4 +33,4 @@ class CreateSubstancesTable extends Migration
     {
         Schema::dropIfExists('substances');
     }
-}
+};
--- database/migrations/2021_04_27_202815_create_substance_hazards_table.php
+++ database/migrations/2021_04_27_202815_create_substance_hazards_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSubstanceHazardsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -30,4 +30,4 @@ class CreateSubstanceHazardsTable extends Migration
     {
         Schema::dropIfExists('substance_hazards');
     }
-}
+};
