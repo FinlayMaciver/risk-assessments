03b7be09870981e7ba1aed770c12e3784bcef520
Shift <shift@laravelshift.com>
2022-05-04 12:40:50 +0000

    Remove unnecessary `$model` property

--- database/factories/FormFactory.php
+++ database/factories/FormFactory.php
@@ -13,13 +13,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class FormFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Form::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array
--- database/factories/MicroOrganismFactory.php
+++ database/factories/MicroOrganismFactory.php
@@ -10,13 +10,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class MicroOrganismFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = MicroOrganism::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array
