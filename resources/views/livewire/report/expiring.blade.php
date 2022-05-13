e5c44e7ea4edcfb7f6d8306250c7c08d1dd34b3e
Ohffs <ohffsnotnow@gmail.com>
2022-05-13 14:21:27 +0100

    Make expiring table a bit more mobile-friendly

--- resources/views/livewire/report/expiring.blade.php
+++ resources/views/livewire/report/expiring.blade.php
@@ -14,36 +14,39 @@
                     </div>
                 </div>
                 <hr />
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Management Unit</th>
-                            <th>Location</th>
-                            <th>Review Date</th>
-                            <th>Type</th>
-                            <th>Status</th>
-                            <th>Created By</th>
-                            <th>Reviewer</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($forms as $form)
+                <div class="table-responsive">
+
+                    <table class="table">
+                        <thead>
                             <tr>
-                                <td>
-                                    <a href="{{ route('form.show', $form->id) }}">{{ str($form->title)->substr(0, 32) }}</a>
-                                </td>
-                                <td>{{ $form->management_unit }}</td>
-                                <td>{{ $form->location }}</td>
-                                <td>{{ $form->review_date?->format('d/m/Y') }}</td>
-                                <td>{{ $form->type }}</td>
-                                <td>{{ $form->status }}</td>
-                                <td>{{ $form->user?->full_name }}</td>
-                                <td>{{ $form->supervisor?->full_name }}</td>
+                                <th>Title</th>
+                                <th>Management Unit</th>
+                                <th>Location</th>
+                                <th>Review Date</th>
+                                <th>Type</th>
+                                <th>Status</th>
+                                <th>Created By</th>
+                                <th>Reviewer</th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            @foreach ($forms as $form)
+                                <tr id="form-row-{{ $form->id }}">
+                                    <td>
+                                        <a href="{{ route('form.show', $form->id) }}">{{ str($form->title)->substr(0, 32) }}</a>
+                                    </td>
+                                    <td>{{ $form->management_unit }}</td>
+                                    <td>{{ $form->location }}</td>
+                                    <td>{{ $form->review_date?->format('d/m/Y') }}</td>
+                                    <td>{{ $form->type }}</td>
+                                    <td>{{ $form->status }}</td>
+                                    <td>{{ $form->user?->full_name }}</td>
+                                    <td>{{ $form->supervisor?->full_name }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
